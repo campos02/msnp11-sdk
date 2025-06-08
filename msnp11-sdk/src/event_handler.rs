9cbc6c8fe122a64533bc82eb99fc1adbc31e2d8e
campos <nonesovile96@hotmail.com>
2025-06-08 15:27:12 -0300

    Export event handler trait with foreign

--- msnp11-sdk/src/event_handler.rs
+++ msnp11-sdk/src/event_handler.rs
@@ -2,7 +2,7 @@ use crate::event::Event;
 
 /// This trait is used to define an async event handler when using this SDK through foreign language bindings. If using it with Rust
 /// the preferred handling method is closures.
-#[uniffi::export]
+#[uniffi::export(with_foreign)]
 #[async_trait::async_trait]
 pub trait EventHandler: Send + Sync {
     async fn handle(&self, event: Event);
