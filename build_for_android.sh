1d7b90863cefa1daf826c436d773f0f911a7d310
campos <nonesovile96@hotmail.com>
2025-06-22 20:12:12 -0300

    Use cargo ndk to build for android

--- build_for_android.sh
+++ build_for_android.sh
@@ -1,14 +1,5 @@
 cd msnp11-sdk || exit
-cross build --release --target x86_64-linux-android
-cross build --release --target i686-linux-android
-cross build --release --target armv7-linux-androideabi
-cross build --release --target aarch64-linux-android
+cargo ndk -t arm64-v8a -t armeabi-v7a -t x86_64 -t x86 -o ./jniLibs build --release
 
 cd ..
-cargo run -p uniffi-bindgen generate --library target/x86_64-linux-android/release/libmsnp11_sdk.so --language kotlin --out-dir out
-
-cd out || exit
-cp ../target/x86_64-linux-android/release/libmsnp11_sdk.so ./jniLibs/x86_64
-cp ../target/i686-linux-android/release/libmsnp11_sdk.so ./jniLibs/x86
-cp ../target/armv7-linux-androideabi/release/libmsnp11_sdk.so ./jniLibs/armeabi-v7a
-cp ../target/aarch64-linux-android/release/libmsnp11_sdk.so ./jniLibs/arm64-v8a
+cargo run -p uniffi-bindgen generate --library target/aarch64-linux-android/release/libmsnp11_sdk.so --language kotlin --out-dir out
