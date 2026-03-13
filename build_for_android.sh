557a213aa7a505e9c6b6c6fadb9ac65d9655c8c2
campos <45215327+campos02@users.noreply.github.com>
2026-03-13 13:14:32 -0300

    Add --all-features to build_for_android.sh

--- build_for_android.sh
+++ build_for_android.sh
@@ -1,5 +1,5 @@
 cd msnp11-sdk || exit
-cargo ndk -t arm64-v8a -t armeabi-v7a -t x86_64 -t x86 -o ./jniLibs build --release
+cargo ndk -t arm64-v8a -t armeabi-v7a -t x86_64 -t x86 -o ./jniLibs build --release --all-features
 
 cd ..
 cargo run -p uniffi-bindgen generate --library target/aarch64-linux-android/release/libmsnp11_sdk.so --language kotlin --out-dir out
